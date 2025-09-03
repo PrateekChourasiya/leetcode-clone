15631802d109cf118ae61cf295dc6a75462b73df
Prateek <prateekchourasiya2003@gmail.com>
2025-09-03 23:58:04 +0530

    remove embed/iframe tag

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -294,16 +294,7 @@ const ProblemPage = () => {
                   <div className="prose max-w-none text-gray-300">
                     <h2 className="text-xl font-bold mb-4 text-white">Editorial</h2>
                     <div className="whitespace-pre-wrap text-sm leading-relaxed">
-                      {/* Editorial content will be available after you solve the problem. */}
-                          <iframe
-                          width="400"
-                          height="300"
-                          src="https://www.youtube.com/embed/KLlXCFG5TnA"
-                          title="YouTube video player"
-                          frameborder="0"
-                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                          allowfullscreen>
-                          </iframe>
+                      Editorial content will be available after you solve the problem.
                     </div>
                   </div>
                 )}
