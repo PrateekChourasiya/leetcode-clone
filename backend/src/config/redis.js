7d72049ab6e09b9607c9d5f2fc74c9e9641dc69f
Prateek <prateekchourasiya2003@gmail.com>
2025-11-05 21:53:11 +0530

    added redis db

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,7 +4,7 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-14810.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+        host: 'redis-14035.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
         port: 14810
     }
 });
