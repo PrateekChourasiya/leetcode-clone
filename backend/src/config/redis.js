ea361562afbc05a3e01d12d52361028cab6881db
Faraz-Khan-79996 <farazgod1234@gmail.com>
2025-12-07 23:41:36 +0530

    changed to new redis server.

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-14035.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 14035
+        host : 'redis-14909.c305.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 14909
     }
 });
 
