355d27189920834037013d3347591bc6a1f00e51
Prateek <prateekchourasiya2003@gmail.com>
2025-12-08 01:20:54 +0530

    some fixes in chatAI

--- backend/src/controllers/solveDoubt.js
+++ backend/src/controllers/solveDoubt.js
@@ -1,19 +1,15 @@
 const { GoogleGenAI } = require("@google/genai");
 
+const solveDoubt = async (req, res) => {
+  try {
+    const { messages, title, description, testCases, startCode } = req.body;
 
-const solveDoubt = async(req , res)=>{
+    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_KEY });
 
-
-    try{
-
-        const {messages,title,description,testCases,startCode} = req.body;
-        const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_KEY });
-       
-        async function main() {
-        const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
-        contents: messages,
-        config: {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: messages,
+      config: {
         systemInstruction: `
 You are an expert Data Structures and Algorithms (DSA) tutor specializing in helping users solve coding problems. Your role is strictly limited to DSA-related assistance only.
 
@@ -23,29 +19,22 @@ You are an expert Data Structures and Algorithms (DSA) tutor specializing in hel
 [EXAMPLES]: ${testCases}
 [startCode]: ${startCode}
 
-What you need to do is to do is to take user to the problem solution step by step, by first giving some hints and testcases, then giving some useful approaches and issues and correction in their solution if they provide one, and then finally correct solution if asked.
+What you need to do is to take user to the problem solution step by step, by first giving some hints and testcases, then giving some useful approaches and issues and correction in their solution if they provide one, and then finally correct solution if asked.
 
 Remember: Your goal is to help users learn and understand DSA concepts through the lens of the current problem, so stick to it only, do not entertain any other queries unrelated to current problem context.
-`},
-// thinkingConfig: {
-//         thinkingBudget: -1, // Disable thinking with 0, enable with -1
-//       },
+`,
+      },
     });
-     
-    res.status(201).json({
-        message:response.text
-    });
-    // console.log(response.text);
-    }
 
-    main();
-      
-    }
-    catch(err){
-        res.status(500).json({
-            message: "Internal server error"
-        });
-    }
-}
+    res.status(201).json({
+      message: response.text,
+    });
+  } catch (err) {
+    console.error("Gemini error:", err);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
 
 module.exports = solveDoubt;
